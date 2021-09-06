9a339683d1d97fadc2008315ab10d79488e18ef1
unknown <6804977@device.hmc.co.kr>
2021-09-06 18:02:30 +0900

    client에 연결

--- server.js
+++ server.js
@@ -11,7 +11,32 @@ app.use(cors());
 
 // products의 경로로 get 요청이 왔을 때 아래 함수가 진행됨
 app.get("/products", (req, res) => {
-  res.send("업로드 된 상품들입니다.");
+  // http와는 다르게 express에서는 send안에 객체 넣을 수 있음
+  res.send({
+    product: [
+      {
+        id: 1,
+        name: "농구공",
+        price: 100000,
+        seller: "조던",
+        imageurl: "images/products/basketball1.jpeg",
+      },
+      {
+        id: 2,
+        name: "축구공",
+        price: 500000,
+        seller: "메시",
+        imageurl: "images/products/soccerball1.jpg",
+      },
+      {
+        id: 3,
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageurl: "images/products/keyboard1.jpg",
+      },
+    ],
+  });
 });
 
 app.post("/products", (req, res) => {
